3487f2e8d69e75d04532599633b20050d68684d4
Marc Scholten <marc@digitallyinduced.com>
2026-01-16 10:14:24 +0100

    fixed tests

--- testsuite/tests/ghc-api/annotations-literals/parsed.hs
+++ testsuite/tests/ghc-api/annotations-literals/parsed.hs
@@ -3,7 +3,6 @@
 -- argument.
 module Main where
 
-import GHC.Data.FastString
 import GHC.Types.Basic
 import GHC.Types.SourceText
 import Data.Data
@@ -15,6 +14,7 @@ import GHC.Driver.Ppr
 import GHC.Unit.Types (GenUnit(..), Definite(..))
 import GHC.Utils.Monad
 import GHC.Utils.Outputable
+import GHC.Utils.Encoding (utf8DecodeByteString)
 import GHC.Data.Bag (filterBag,isEmptyBag)
 import System.Directory (removeFile)
 import System.Environment( getArgs )
@@ -49,30 +49,30 @@ testOneFile libdir fileName = do
 
      doHsLit :: HsLit GhcPs -> [String]
      doHsLit (HsChar       (SourceText src) c)
-       = ["HsChar [" ++ unpackFS src ++ "] " ++ show c]
+       = ["HsChar [" ++ utf8DecodeByteString src ++ "] " ++ show c]
      doHsLit (HsCharPrim   (SourceText src) c)
-       = ["HsCharPrim [" ++ unpackFS src ++ "] " ++ show c]
+       = ["HsCharPrim [" ++ utf8DecodeByteString src ++ "] " ++ show c]
      doHsLit (HsString     (SourceText src) c)
-       = ["HsString [" ++ unpackFS src ++ "] " ++ show c]
+       = ["HsString [" ++ utf8DecodeByteString src ++ "] " ++ show c]
      doHsLit (HsStringPrim (SourceText src) c)
-       = ["HsStringPrim [" ++ unpackFS src ++ "] " ++ show c]
+       = ["HsStringPrim [" ++ utf8DecodeByteString src ++ "] " ++ show c]
      doHsLit (HsInt  _     (IL (SourceText src) _ c))
-       = ["HsInt [" ++ unpackFS src ++ "] " ++ show c]
+       = ["HsInt [" ++ utf8DecodeByteString src ++ "] " ++ show c]
      doHsLit (HsIntPrim (SourceText src) c)
-       = ["HsIntPrim [" ++ unpackFS src ++ "] " ++ show c]
+       = ["HsIntPrim [" ++ utf8DecodeByteString src ++ "] " ++ show c]
      doHsLit (HsWordPrim   (SourceText src) c)
-       = ["HsWordPrim [" ++ unpackFS src ++ "] " ++ show c]
+       = ["HsWordPrim [" ++ utf8DecodeByteString src ++ "] " ++ show c]
      doHsLit (HsInt64Prim  (SourceText src) c)
-       = ["HsInt64Prim [" ++ unpackFS src ++ "] " ++ show c]
+       = ["HsInt64Prim [" ++ utf8DecodeByteString src ++ "] " ++ show c]
      doHsLit (HsWord64Prim (SourceText src) c)
-       = ["HsWord64Prim [" ++ unpackFS src ++ "] " ++ show c]
+       = ["HsWord64Prim [" ++ utf8DecodeByteString src ++ "] " ++ show c]
      doHsLit _ = []
 
      doOverLit :: OverLitVal -> [String]
      doOverLit (HsIntegral  (IL (SourceText src) _ c))
-       = ["HsIntegral [" ++ unpackFS src ++ "] " ++ show c]
+       = ["HsIntegral [" ++ utf8DecodeByteString src ++ "] " ++ show c]
      doOverLit (HsIsString  (SourceText src) c)
-       = ["HsIsString [" ++ unpackFS src ++ "] " ++ show c]
+       = ["HsIsString [" ++ utf8DecodeByteString src ++ "] " ++ show c]
      doOverLit _ = []
 
 pp a = showPprUnsafe a
